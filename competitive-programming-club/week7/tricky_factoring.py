81f53ea2fa109637d7940b015370d01ffad9420f
Eric-Tourigny <eric.tourigny1@ucalgary.ca>
2025-10-25 11:21:07 -0600

    Revised tricky factoring

--- competitive-programming-club/week7/tricky_factoring.py
+++ competitive-programming-club/week7/tricky_factoring.py
@@ -4,10 +4,8 @@ a_cur = a
 c_cur = c
 
 cutoff = 10 ** 6
-a_prime_counts = []
-a_prime_factors = []
-c_prime_counts = []
-c_prime_factors = []
+a_prime_factors = {}
+c_prime_factors = {}
 values = list(range(cutoff))
 
 iterator = iter(values)
@@ -20,8 +18,7 @@ for value in iterator:
             while a_cur % value == 0:
                 a_cur /= value
                 count += 1
-            a_prime_factors.append(value)
-            a_prime_counts.append(count)
+            a_prime_factors[value] = count
             if a_cur == 1 and c_cur == 1:
                 break
         if c_cur % value == 0:
@@ -29,8 +26,7 @@ for value in iterator:
             while c_cur % value == 0:
                 c_cur /= value
                 count += 1
-            c_prime_factors.append(value)
-            c_prime_counts.append(count)
+            c_prime_factors[value] = count
             if a_cur == 1 and c_cur == 1:
                 break
         number = value
@@ -38,47 +34,16 @@ for value in iterator:
             values[number] = -1
             number += value
 
-def generate_factors(prime_factors, prime_counts, number):
-    cutoff = int(number ** 0.5)
+z_prime_factors = {}
+for factor, count in a_prime_factors.items():
+    z_prime_factors[factor] = count
+for factor, count in c_prime_factors.items():
+    if factor in z_prime_factors:
+        z_prime_factors[factor] += count
+    else:
+        z_prime_factors[factor] = count
 
-    factors = [1]
-    corresponding_factors = [number]
-    for pf, count in zip(prime_factors, prime_counts):
-        size = len(factors)
-
-        multiplier = pf
-        for i in range(count):
-            for j in range(size):
-                value = factors[j] * multiplier
-                if value <= cutoff:
-                    factors.append(value)
-                    corresponding_factors.append(number / value)
-            multiplier *= pf
-    return factors, corresponding_factors
-
-a_factors, a_corresponding_factors = generate_factors(a_prime_factors, a_prime_counts, a)
-c_factors, c_corresponding_factors = generate_factors(c_prime_factors, c_prime_counts, c)
-
-
-#a_factors = []
-#a_corresponding = []
-#for f in range(1, int(math.sqrt(a)) + 1):
-#    if a % f == 0:
-#        a_factors.append(f)
-#        a_corresponding.append(a // f)
-#
-#c_factors = []
-#c_corresponding = []
-#for f in range(1, int(math.sqrt(c)) + 1):
-#    if c % f == 0:
-#        c_factors.append(f)
-#        c_corresponding.append(c // f)
-
-b_values = set()
-for a1, a2 in zip(a_factors, a_corresponding_factors):
-    for c1 in c_factors:
-        c2 = c / c1
-        b_values.add(a1 * c1 + a2 * c2)
-        b_values.add(a1 * c2 + a2 * c1)
-
-print(len(b_values) * 2)
+total = 1
+for factor, count in z_prime_factors.items():
+    total *= count + 1
+print(total) 
