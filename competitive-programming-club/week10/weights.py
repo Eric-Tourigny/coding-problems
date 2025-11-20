20a0894262a80e176378481a482bc6bb8c02d91f
Eric-Tourigny <eric.tourigny1@ucalgary.ca>
2025-11-20 14:05:54 -0700

    Fixes CPC week 10 weights

--- competitive-programming-club/week10/weights.py
+++ competitive-programming-club/week10/weights.py
@@ -1,40 +1,28 @@
-import bisect
-
 n = int(input())
 the_weights = list(sorted(int(input()) for _ in range(n)))
-size = len(the_weights)
+UPPER_LIMIT = 2000
+elements = [False] * UPPER_LIMIT
+elements[0] = True
 
-def abs_min(a, b):
-    if abs(a) == abs(b):
-        if a > 0:
-            return a
-        else:
-            return b
-    elif abs(a) < abs(b):
-        return a
-    else:
-        return b
+for weight in the_weights:
+    for i, element in enumerate(list(elements)):
+        if element:
+            if i + weight < UPPER_LIMIT:
+                elements[i + weight] = True
 
-def recursive_solve(target: int, i: int):
-    value = the_weights[i]
-    new_target = target - value
-    if new_target < 0:
-        return -new_target
+for u in range(1000):
+    if elements[u + 1000]:
+        break
+else:
+    u = 10000
 
-    next_larger = bisect.bisect_right(the_weights, new_target, 0, i)
-        
-    if next_larger < i:
-        best_result  = the_weights[next_larger] - new_target
-    else:
-        best_result = 10000
+for l in range(1000):
+    if elements[1000 - l]:
+        break
+else:
+    l = 10000
 
-    if next_larger == 0:
-        best_result = abs_min(best_result, value - target)
-    else:
-        best_result = abs_min(best_result, recursive_solve(target - value, next_larger - 1))
-
-        best_result = abs_min(best_result, recursive_solve(target, i - 1))
-        
-    return best_result
-
-print(1000 + recursive_solve(1000, size - 1))
+if u <= l:
+    print(1000 + u)
+else:
+    print(1000 - l)
